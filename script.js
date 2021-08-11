7fcd804db87a5c0f9baa95f9cfcb6a564dd69ea8
ASkauen <andreas@rubynor.com>
2021-08-11 15:10:57 +0200

    Made layout for calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(x, y) {
+  console.log(x + y)
+}
+
+function subtract(x, y) {
+  console.log(x - y)
+}
+
+function divide(x, y) {
+  console.log(x / y)
+}
+
+function multiply(x, y) {
+  console.log(x * y)
+}
+
+function operate(x, y, operator) {
+  console.log(operator(x, y))
+}
